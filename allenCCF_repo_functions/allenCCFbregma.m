3684ed373f2c1742be36565b8f08c59ff37f0ce0
fjflores <fjflores@mit.edu>
2022-01-10 10:45:47 -0500

    update DV bregma location to the on in neuropix_traj_explorer.

--- allenCCF_repo_functions/allenCCFbregma.m
+++ allenCCF_repo_functions/allenCCFbregma.m
@@ -5,4 +5,4 @@ function bregma = allenCCFbregma()
 % Return coordinates of bregma in allen CCF 10um volume coordinates
 % This is just an estimate by me, the only one that's definitely right is
 % the third dimension which is the midline
-bregma = [540 0 570]; % AP, DV, LR
+bregma = [540 44 570]; % AP, DV, LR
