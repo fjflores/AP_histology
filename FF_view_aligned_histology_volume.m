9f8f8747f12f6b8e9847100fc8ef89ea327af925
Francisco J. Flores <fjflores@mit.edu>
2022-01-24 18:10:25 -0500

    tidy up things.

--- FF_view_aligned_histology_volume.m
+++ FF_view_aligned_histology_volume.m
@@ -1,4 +1,4 @@
-function FF_view_aligned_histology_volume(tv,av,slice_im_path,channel,thr,coords)
+function FF_view_aligned_histology_volume(av,slice_im_path,channel,thr,coords)
 % AP_view_aligned_histology_volume(tv,av,st,slice_im_path,channel)
 %
 % Plot histology warped onto CCF volume
@@ -8,7 +8,6 @@ function FF_view_aligned_histology_volume(tv,av,slice_im_path,channel,thr,coords
 
 % Initialize guidata
 gui_data = struct;
-gui_data.tv = tv;
 gui_data.av = av;
 
 % Load in slice images
@@ -58,24 +57,26 @@ gui_fig = figure;
 switch coords
     case 'ccf'
         axes_atlas = axes;
-        [~, brain_outline] = plotBrainGrid([],axes_atlas);
+        plotBrainGrid([],axes_atlas);
         set(axes_atlas,'ZDir','reverse');
         hold(axes_atlas,'on');
         axis vis3d equal on manual
         caxis([0 300]);
-        [ap_max,dv_max,ml_max] = size(tv);
+        [ap_max,dv_max,ml_max] = size(av);
         xlim([-10,ap_max+10])
         ylim([-10,ml_max+10])
         zlim([-10,dv_max+10])
+        view([-30,25]);
         
     case 'pax'
         axes_atlas = plotBrainSurf( av );
+        view( [ -120, 25 ] );
         
     otherwise
         error( 'Coordinates must be either ''ccf'' or ''pax''.' )
         
 end
-view([-30,25]);
+
 
 
 switch channel
@@ -151,48 +152,6 @@ for curr_slice = 1:length(gui_data.slice_im)
     end
     
 end
-
-
-% % Attempt plotting as 3D surface
-
-% keyboard
-
-% thresh_volume = false(size(tv));
-%
-% for curr_slice = 1:length(gui_data.slice_im)
-%
-%     % Get thresholded image
-%     curr_slice_im = gui_data.atlas_aligned_histology{curr_slice}(:,:,channel);
-%     slice_alpha = curr_slice_im;
-%     slice_alpha(slice_alpha < 100) = 0;
-%
-%     slice_thresh = curr_slice_im > 200;
-%
-%     slice_thresh_ap = round(gui_data.histology_ccf(curr_slice).plane_ap(slice_thresh));
-%     slice_thresh_dv = round(gui_data.histology_ccf(curr_slice).plane_dv(slice_thresh));
-%     slice_thresh_ml = round(gui_data.histology_ccf(curr_slice).plane_ml(slice_thresh));
-%
-%     thresh_idx = sub2ind(size(tv),slice_thresh_ap,slice_thresh_dv,slice_thresh_ml);
-%     thresh_volume(thresh_idx) = true;
-%
-% end
-%
-% thresh_volume_dilate = imdilate(thresh_volume,strel('sphere',5));
-%
-% sphere_size = (4/3)*pi*5^3;
-% a = bwareaopen(thresh_volume_dilate,round(sphere_size*20));
-% b = imdilate(a,strel('sphere',5));
-% c = imresize3(+b,1/10,'nearest');
-%
-% ap = linspace(1,size(tv,1),size(c,1));
-% dv = linspace(1,size(tv,2),size(c,2));
-% ml = linspace(1,size(tv,3),size(c,3));
-%
-% figure;
-% plotBrainGrid([],gca); hold on;
-% isosurface(ap,ml,dv,permute(c,[3,1,2]));
-% camlight;
-
 
 
 
