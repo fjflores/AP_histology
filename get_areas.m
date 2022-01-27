f6ee194a0afb2ee3415f337913d49317d2d96723
Francisco J. Flores <fjflores@mit.edu>
2022-01-27 18:53:29 -0500

    add working fx to get area at the probe tip.

---
+++ get_areas.m
@@ -0,0 +1,9 @@
+function get_areas( probe_fit )
+
+ccf_points_cat = round(cell2mat(ccf_points));
+% Get indicies from subscripts
+ccf_points_idx = sub2ind(size(av),ccf_points_cat(:,1),ccf_points_cat(:,2),ccf_points_cat(:,3));
+% Find annotated volume (AV) values at points
+ccf_points_av = av(ccf_points_idx);
+% Get areas from the structure tree (ST) at given AV values
+ccf_points_areas = st(ccf_points_areas,:).safe_name;
