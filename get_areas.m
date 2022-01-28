93b49d94ef8350f6eb38f19bb05d1a8c8481e747
fjflores <fjflores@mit.edu>
2022-01-28 11:45:28 -0500

    find area at the tip of the electrode.

--- get_areas.m
+++ get_areas.m
@@ -1,9 +1,21 @@
-function get_areas( probe_fit )
+function ccf_area = get_areas( probe_fit, av, st )
 
-ccf_points_cat = round(cell2mat(ccf_points));
-% Get indicies from subscripts
-ccf_points_idx = sub2ind(size(av),ccf_points_cat(:,1),ccf_points_cat(:,2),ccf_points_cat(:,3));
+% find row with max depth
+[ ~, maxIdx] = max( probe_fit( :, 3 ) );
+
+
+bottomPts = probe_fit( maxIdx, : );
+ccf_points_cat = round( bottomPts );
+
+% Get indicies from subscripts. Remember ccf is AP, DV, ML.
+ccf_points_idx = sub2ind(...
+    size( av ),...
+    ccf_points_cat( :, 1 ),...
+    ccf_points_cat( :, 3 ),...
+    ccf_points_cat( :, 2 ) );
+
 % Find annotated volume (AV) values at points
-ccf_points_av = av(ccf_points_idx);
+ccf_points_av = av( ccf_points_idx );
+
 % Get areas from the structure tree (ST) at given AV values
-ccf_points_areas = st(ccf_points_areas,:).safe_name;
+ccf_area = st( ccf_points_av, : ).safe_name{ 1 };
--- plot_probe.m
+++ plot_probe.m
@@ -1,4 +1,4 @@
-function probe_fit_line = plot_probe( av, probe_ccf, coords, st )
+function ccf_areas = plot_probe( av, probe_ccf, coords, st )
 % PLOT_PROBE plots histologically-defined probe points within Allen CCF.
 %
 % Usage:
@@ -7,8 +7,8 @@ function probe_fit_line = plot_probe( av, probe_ccf, coords, st )
 % Input:
 % av: annotated volume data from Allen CCF.
 % probe_ccf: probe location data from AP_get_probe_histology.
-% coords: Optional. Defines what coordinates to use. If 'ccf' uses the 
-%         original common coordinate framework and a brain grid. If 'pax', 
+% coords: Optional. Defines what coordinates to use. If 'ccf' uses the
+%         original common coordinate framework and a brain grid. If 'pax',
 %         transforms the probe ccf coordinates to paxinos and plots a brain
 %         surface. Default 'pax'.
 % st: Optional. If passed, plots the brain areas spanned by the probe.
@@ -34,12 +34,12 @@ if nargin == 4
     
 end
 
-% Plot probe trajectories
+% Plot either brain grid or surface.
 switch coords
     case 'ccf'
         figure( 'Name','Probe trajectories' );
         axes_atlas = axes;
-        [ ~, brain_outline ] = plotBrainGrid( [], axes_atlas );
+        plotBrainGrid( [], axes_atlas );
         set( axes_atlas, 'ZDir', 'reverse' );
         hold( axes_atlas, 'on' );
         axis vis3d equal off manual
@@ -49,7 +49,7 @@ switch coords
         xlim( [ -10, ap_max + 10 ] )
         ylim( [ -10, ml_max + 10 ] )
         zlim( [ -10, dv_max + 10 ] )
-        h = rotate3d( gca );
+        h = rotate3d( axes_atlas );
         h.Enable = 'on';
         
     case 'pax'
@@ -58,10 +58,10 @@ switch coords
         set( axes_atlas, 'ZDir', 'reverse' );
         hold( axes_atlas, 'on' );
         axis vis3d equal off manual
-        view( [ -30, 25 ] );
+        view( [ -120, 25 ] );
         h = rotate3d( axes_atlas );
         h.Enable = 'on';
-        probe_ccf = trprobeccf( probe_ccf );
+        probe_pax = trprobeccf( probe_ccf );
         
     otherwise
         error( 'Must provide valid coordinates, either ''pax'' or ''ccf''.' )
@@ -69,11 +69,18 @@ switch coords
 end
 
 n_probes = length( probe_ccf );
-probe_fit_line = cell( 1, n_probes );
+probe_fit = cell( 1, n_probes );
 for curr_probe = 1 : n_probes
-    thisPoints = probe_ccf( curr_probe ).points;
+    switch coords
+        case 'ccf'
+            thisPoints = probe_ccf( curr_probe ).points;
+            
+        case 'pax'
+            thisPoints = probe_pax( curr_probe ).points;
+            
+    end
     xyz = [ thisPoints( :, 1 ), thisPoints( :, 3 ), thisPoints( :, 2 ) ];
-    probe_fit_line{ curr_probe } = fit3d( xyz );
+    probe_fit{ curr_probe } = fit3d( xyz );
     
     % Plot points and line of best fit
     plot3(...
@@ -84,9 +91,9 @@ for curr_probe = 1 : n_probes
         'color', probe_ccf( curr_probe ).probe_color,...
         'MarkerSize', 20 );
     line(...
-        probe_fit_line{ curr_probe }( :, 1 ),...
-        probe_fit_line{ curr_probe }( :, 2 ),...
-        probe_fit_line{ curr_probe }( :, 3 ),...
+        probe_fit{ curr_probe }( :, 1 ),...
+        probe_fit{ curr_probe }( :, 2 ),...
+        probe_fit{ curr_probe }( :, 3 ),...
         'color', probe_ccf( curr_probe ).probe_color,...
         'linewidth', 2 )
     
@@ -94,37 +101,47 @@ end
 
 
 % Plot probe areas
-if areas
-    figure( 'Name', 'Trajectory areas' );
-    % (load the colormap - located in the repository, find by associated fcn)
-    allenCCF_path = fileparts( which( 'allenCCFbregma' ) );
-    cmap_filename = [ allenCCF_path filesep 'allen_ccf_colormap_2017.mat' ];
-    load( cmap_filename );
-    
-    for curr_probe = 1 : n_probes
-        curr_axes = subplot( 1, n_probes, curr_probe );
-        trajectory_area_boundaries = ...
-            [ 1;...
-            find( diff( probe_ccf( curr_probe ).trajectory_areas ) ~= 0 );...
-            length( probe_ccf( curr_probe ).trajectory_areas ) ];
-        trajectory_area_centers =...
-            trajectory_area_boundaries( 1 : end - 1) +...
-            diff( trajectory_area_boundaries ) / 2;
-        areaCent = round( trajectory_area_centers );
-        trajectory_area_labels =...
-            st.safe_name(...
-            probe_ccf( curr_probe ).trajectory_areas( areaCent ) );
-        
-        image( probe_ccf( curr_probe ).trajectory_areas );
-        colormap( curr_axes, cmap );
-        caxis( [ 1, size( cmap, 1 ) ] )
-        set( curr_axes,...
-            'YTick', trajectory_area_centers,...
-            'YTickLabels', trajectory_area_labels );
-        set(curr_axes,'XTick',[]);
-        title(['Probe ' num2str(curr_probe)]);
-        
-    end
+% if areas
+%     figure( 'Name', 'Trajectory areas' );
+%     % (load the colormap - located in the repository, find by associated fcn)
+%     allenCCF_path = fileparts( which( 'allenCCFbregma' ) );
+%     cmap_filename = [ allenCCF_path filesep 'allen_ccf_colormap_2017.mat' ];
+%     load( cmap_filename );
+%     
+%     for curr_probe = 1 : n_probes
+%         curr_axes = subplot( 1, n_probes, curr_probe );
+%         trajectory_area_boundaries = ...
+%             [ 1;...
+%             find( diff( probe_ccf( curr_probe ).trajectory_areas ) ~= 0 );...
+%             length( probe_ccf( curr_probe ).trajectory_areas ) ];
+%         trajectory_area_centers =...
+%             trajectory_area_boundaries( 1 : end - 1) +...
+%             diff( trajectory_area_boundaries ) / 2;
+%         areaCent = round( trajectory_area_centers );
+%         trajectory_area_labels =...
+%             st.safe_name(...
+%             probe_ccf( curr_probe ).trajectory_areas( areaCent ) );
+%         
+%         image( probe_ccf( curr_probe ).trajectory_areas );
+%         colormap( curr_axes, cmap );
+%         caxis( [ 1, size( cmap, 1 ) ] )
+%         set( curr_axes,...
+%             'YTick', trajectory_area_centers,...
+%             'YTickLabels', trajectory_area_labels );
+%         set(curr_axes,'XTick',[]);
+%         title(['Probe ' num2str(curr_probe)]);
+%         
+%     end
+%     
+% end
+
+% get areas at the tip of the probe
+ccf_areas = cell( n_probes, 1 );
+for curr_probe = 1 : n_probes
+    thisPoints = probe_ccf( curr_probe ).points;
+    xyz = [ thisPoints( :, 1 ), thisPoints( :, 3 ), thisPoints( :, 2 ) ];
+    probe_fit = fit3d( xyz );
+    ccf_areas{ curr_probe, 1 } = get_areas( probe_fit, av, st );
     
 end
 
@@ -133,9 +150,9 @@ function xyzEst = fit3d( xyz )
 xyzHat = mean( xyz, 1 );
 A = xyz - xyzHat;
 N = length( A );
-C = ( A' * A ) / ( N - 1 ); 
+C = ( A' * A ) / ( N - 1 );
 [ R, ~, ~ ] = svd( C, 0 );
-x = A * R( :, 1 );    % project residuals on R(:,1) 
+x = A * R( :, 1 );    % project residuals on R(:,1)
 xMin = min( x );
 xMax = max( x );
 dx = xMax - xMin;
