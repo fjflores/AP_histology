a3bbb4cfeec00a98d2ec7513bd4ea6226bb64774
Francisco J. Flores <fjflores@mit.edu>
2022-02-15 17:53:20 -0500

    add some other fields to the areaCCF structure.

--- get_areas.m
+++ get_areas.m
@@ -20,9 +20,13 @@ ccf_points_av = av( ccf_points_idx );
 % Get areas from the structure tree (ST) at given AV values
 areaCCF.name = st( ccf_points_av, : ).safe_name{ 1 };
 areaCCF.acronym = st( ccf_points_av, : ).acronym{ 1 };
-areaCCF.sphinx_id = st( ccf_points_av, : ).sphinx_id( 1 );
-areaCCF.parent_id = st( ccf_points_av, : ).parent_structure_id( 1 );
-rgb = hex2rgb( st( ccf_points_av, : ).color_hex_triplet{ 1 } );
+areaCCF.id = st( ccf_points_av, : ).id;
+areaCCF.atlas_id = st( ccf_points_av, : ).atlas_id;
+areaCCF.sphinx_id = st( ccf_points_av, : ).sphinx_id;
+areaCCF.parent_id = st( ccf_points_av, : ).parent_structure_id;
+areaCCF.structure_id_path = st( ccf_points_av, : ).structure_id_path{ 1 };
+areaCCF.color_hex = st( ccf_points_av, : ).color_hex_triplet{ 1 };
+rgb = hex2rgb( areaCCF.color_hex );
 areaCCF.color_rgb = rgb ./ 255;
 
 
