1721ed86b4a1dd8cc743abc51ed2a24fa4b3c150
Francisco J. Flores <fjflores@mit.edu>
2022-07-07 17:34:13 -0400

    fix a bug in the regexp.

--- hex2rgb.m
+++ hex2rgb.m
@@ -1,7 +1,19 @@
 function rgb = hex2rgb( hex )
+% HEX2RGB converts hex color triplets to rgb triplets.
+% 
+% Usage:
+% rgb = hex2rgb( hex )
+% 
+% Input:
+% hex:  a character array describing a hexadecimal color triplet. 
+%       E.g.: 'FF909F'.
+% 
+% Output: 
+% rgb: an RGB triplet. E.g. [ 255 144 159 ].
 
-match = regexp( hex, '([A-F]{2})', 'match' );
-for i = 1 : numel( match )
-    rgb( 1, i ) = hex2dec( match{ i } );
+eachHex = regexp( hex, '[0-9A-F]{2}', 'match' );
+rgb = nan( 1, 3 );
+for i = 1 : 3
+    rgb( 1, i ) = hex2dec( eachHex{ i } );
     
 end
