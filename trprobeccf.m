3300c847be7c952110391c5a1b6c1a5467a8c0ec
Isabella Dalla Betta <idallabe@mit.edu>
2022-11-10 16:48:41 -0500

    removed ff's functions from this repo (moved to EA_in_vivo\EA_histology)

--- trprobeccf.m
+++
@@ -1,9 +0,0 @@
-function probe_ccf = trprobeccf( probe_ccf )
-
-nProbes = length( probe_ccf );
-for probeIdx = 1 : nProbes
-    oldPoints = probe_ccf( probeIdx ).points;
-    points = ccf2pax( oldPoints );
-    probe_ccf( probeIdx ).points = points;
-
-end
