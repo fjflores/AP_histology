0baae7f574fb726d476ebaa7dea017710b99a470
Isabella Dalla Betta <idallabe@mit.edu>
2022-11-09 20:25:34 -0500

    added clarifying note to ccf2pax

--- ccf2pax.m
+++ ccf2pax.m
@@ -11,6 +11,12 @@ function trPoints = ccf2pax( points )
 % Output:
 % trPoitns: 3 x n matrix of histology points in paxinos coordinates.
 
+% NOTE
+% Paxinos coordinates are in mm and relative to bregma, where:
+% X = ML = left is negative, right is postivie
+% Y = DV = dorsal is negative, ventral is positive
+% Z = AP = posterior is negative, anterior is positive
+
 bregma = allenCCFbregma();
 x = ( points( :, 1 ) - bregma( 1 ) ) / 100; % AP
 z = ( ( points( :, 2 ) - bregma( 2 ) ) / 100 ) * 0.945; % DV
@@ -20,3 +26,5 @@ trPoints = [ y z -x ];
 
 warning( [ 'Converted to Paxinos coordinates where negative ML values',...
     ' are in left hemisphere.' ] )
+
+end
